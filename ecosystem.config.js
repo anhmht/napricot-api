1057359403a9bb000a0efca26f03e967c95e853c
Tri Anh <anhmht@gmail.com>
2024-07-09 15:27:55 +0700

    remove pm2 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,8 +7,8 @@ module.exports = {
       watch_delay: 1000,
       // Specify which folder to ignore
       ignore_watch: ['node_modules'],
-      instances: '2',
-      exec_mode: 'cluster',
+      // instances: 'max',
+      // exec_mode: 'cluster',
       env: {
         NODE_TLS_REJECT_UNAUTHORIZED: '0'
       }
