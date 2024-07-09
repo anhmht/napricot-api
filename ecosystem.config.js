c46f76cc7e18e49f99d88038b5d75a6397cac77a
Tri Anh <anhmht@gmail.com>
2024-07-09 15:25:43 +0700

    add pm2 instance

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
       watch_delay: 1000,
       // Specify which folder to ignore
       ignore_watch: ['node_modules'],
-      instances: 'max',
+      instances: '2',
       exec_mode: 'cluster',
       env: {
         NODE_TLS_REJECT_UNAUTHORIZED: '0'
