d01855649187ef0e88f8b7109291a212b513b18f
Mai Hoàng Trí Anh <anhmht@gmail.com>
2024-11-01 22:15:54 +0700

    implement url verification

--- controllers/slack.js
+++ controllers/slack.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 
 const clearCloudflareCached = async (req, res, next) => {
+  const { challenge } = req.body
   try {
     const { data } = await axios.post(
       `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/purge_cache`,
@@ -15,7 +16,7 @@ const clearCloudflareCached = async (req, res, next) => {
       }
     )
     res.status(200).json({
-      success: data.success
+      challenge
     })
   } catch (error) {
     return next(error)
