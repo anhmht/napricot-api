7aadff8d6a4fd92a95c2064c919baf679ef6e754
Mai Hoàng Trí Anh <anhmht@gmail.com>
2024-11-01 23:44:16 +0700

    post message to slack channel

--- controllers/slack.js
+++ controllers/slack.js
@@ -1,7 +1,15 @@
 const axios = require('axios')
 
 const clearCloudflareCached = async (req, res, next) => {
-  console.log(req.body)
+  const {
+    event: { bot_id, channel }
+  } = req.body
+  /*
+   * bot: Heroku ChatOps
+   * channel: #narpicot-web-build
+   */
+  if (bot_id !== 'B07AW7M5XV2' && channel !== 'C07BGFT0U5N')
+    return res.status(200).json({ success: true })
 
   try {
     const { data } = await axios.post(
@@ -13,6 +21,37 @@ const clearCloudflareCached = async (req, res, next) => {
         headers: {
           'Content-Type': 'application/json',
           Authorization: `Bearer ${process.env.CLOUDFLARE_API_TOKEN}`
+        }
+      }
+    )
+    if (!data.success) {
+      return res.status(200).json({
+        success: true
+      })
+    }
+
+    await axios.post(
+      `https://hooks.slack.com/services/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`,
+      {
+        attachments: [
+          {
+            color: '#3ea556',
+            blocks: [
+              {
+                type: 'section',
+                text: {
+                  type: 'plain_text',
+                  text: 'Clear cache from Cloudflare. :white_check_mark:',
+                  emoji: true
+                }
+              }
+            ]
+          }
+        ]
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json'
         }
       }
     )
