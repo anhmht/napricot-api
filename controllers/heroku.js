52a94b91c1ed71a69148b711c616c9812fbd967d
Mai Hoàng Trí Anh <anhmht@gmail.com>
2024-11-03 22:45:55 +0700

    send slack message + auto deploy when update post

---
+++ controllers/heroku.js
@@ -0,0 +1,32 @@
+const axios = require('axios')
+const { sendSlackMessage } = require('./slack')
+
+const deploy = async () => {
+  try {
+    // Deploy master branch
+    await axios.post(
+      `https://kolkrabbi.heroku.com/apps/${process.env.HEROKU_APP_ID}/github/push`,
+      {
+        branch: 'master'
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${process.env.HEROKU_ACCESS_TOKEN || ''}`
+        }
+      }
+    )
+
+    await sendSlackMessage({
+      channel: process.env.SLACK_WEBHOOK_WEB_BUILD,
+      message: 'Deploying master branch to Heroku. :rocket:',
+      type: 'INFO'
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+module.exports = {
+  deploy
+}
