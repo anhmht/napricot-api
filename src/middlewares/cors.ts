51303c9bf65155aa783dd2231c32c18196905ec8
Mai Hoàng Trí Anh <anhmht@gmail.com>
2025-06-23 12:18:36 +0700

    test cors

--- src/middlewares/cors.ts
+++ src/middlewares/cors.ts
@@ -9,11 +9,18 @@ import { config } from '../config/env'
  * Configures and returns the CORS middleware
  */
 export function configureCors() {
+  // Log the whitelist for debugging
+  console.log('CORS Whitelist:', config.whitelist)
+
   const corsOptions: cors.CorsOptions = {
     origin: (origin: string | undefined, callback) => {
+      console.log('CORS Origin check:', origin, 'Whitelist:', config.whitelist)
+
       if (config.whitelist.indexOf(origin ?? '') !== -1 || !origin) {
+        console.log('CORS: Origin allowed')
         callback(null, true)
       } else {
+        console.log('CORS: Origin blocked')
         callback(new Error('Not allowed by CORS'))
       }
     }
